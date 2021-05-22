fac94efd5796014dac30dc6c498907e55c38e55b
superbskill <topdev124@gmail.com>
2021-05-23 03:43:17 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './css/tailwind.css';
+
+
+import Header from './components/Header.js';
+import Home from './components/Home.js';
+import About from './components/About.js';
+import Contact from './components/Contact.js';
+
+const App = () => (
+  <div>
+    <Header/>
+  </div>
+);
+
+export default App;
