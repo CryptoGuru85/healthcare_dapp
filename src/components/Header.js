8d11c0710f16915ada431411d43a28532297ddea
storofsoft <storyofsoft1024@gmail.com>
2021-06-16 14:36:00 +0100

    frontend_init

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {  Button, message } from 'antd';
+import { connect } from "react-redux";
+import { logout } from "../actions/auth";
+import { Layout } from 'antd';
 
-import { Link } from 'react-router-dom';
+const { Header } = Layout;
 
-const Header = () => (
-  <header className="bg-gray-100 p-6">
-    <div className="flex items-center justify-between flex-wrap">
-      <div className="block">
-        <Link to="/"><span className="font-semibold text-xl tracking-tight text-red-800">Title Goes Here</span></Link>
-      </div>
-      <nav className="block">
-        <Link to="/"><span className="inline-block text-gray-800 hover:text-gray-600 mr-4">Home</span></Link>
-        <Link to="/about"><span className="inline-block text-gray-800 hover:text-gray-600 mr-4">About</span></Link>
-        <Link to="/contact"><span className="inline-block text-gray-800 hover:text-gray-600 mr-4">Contact</span></Link>
-        <Link to="/"><span className="inline-block px-4 py-2 leading-none border rounded text-gray-600 border-gray-600 hover:text-gray-900 hover:border-gray-900">Login</span></Link>
-      </nav>
-    </div>
-  </header>
-);
+class CustomHeader extends Component {
 
-export default Header;
+    constructor(props){
+        super(props);
+    }
+
+    // async registerDoctor(type) {
+    //     let { healthRecord, web3 } = this.props.global_vars;
+    //     let txn = await healthRecord.signupDoctor
+    //     .sendTransaction("abc",{"from":web3.eth.accounts[0]});
+        
+    //     if(txn)
+    //         message.success("registered successfully as doctor");
+    //     else
+    //         message.error("registered unsuccessfull");
+    // }
+
+    // async registerPatient() {
+    //     let { healthRecord, web3 } = this.props.global_vars;
+    //     let txn = await healthRecord.signupPatient
+    //     .sendTransaction("def", 20 ,{"from":web3.eth.accounts[0]});
+        
+    //     if(txn)
+    //         message.success("registered successfully as patient");
+    //     else
+    //         message.error("registered unsuccessfull");        
+    // }
+
+    componentDidMount() {
+        let { token } = this.props.auth;
+        
+        if(!token)
+            this.props.history.push('/login');
+        else
+            this.props.history.push('/home');
+    }
+
+    render() {
+        let { token, name } = this.props.auth;
+        
+        return (
+            <Header>
+                <div style={{display:"flex", flexDirection:"row" ,justifyContent:"space-between"}}> 
+                    {
+                        token ? 
+                        <div>
+                            <h3 style={{color:"white"}}>Welcome {name}</h3>
+                        </div>: ""
+                    }
+                    {
+                        token ?
+                        <div>
+                            <Button type="primary" onClick={()=>{this.props.logout();this.props.history.push('./login');}} htmlType="submit" className="login-form-button">
+                                Logout
+                            </Button>
+                        </div>:""
+                    }
+                </div>
+            </Header>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+      auth: state.auth,
+      global_vars: state.global_vars
+    };
+};
+
+const actionCreators = {
+    logout
+}
+//export default Header;
+export default connect(mapStateToProps, actionCreators)(CustomHeader);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import {Provider} from "react-redux";
+//import {Router, browserHistory} from "react-router";
+import store from "./utils/store";
+import Routes from "./routes";
 
-ReactDOM.render(<BrowserRouter> <App /> </BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Routes/>
+  </Provider>,
+  document.getElementById('root')
+);
+
+//<App />,
